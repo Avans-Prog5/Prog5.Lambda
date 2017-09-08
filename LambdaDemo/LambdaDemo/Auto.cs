c9c95aebe2387ffe68fa9c1beceb2d3ca8b6359b
Stino <linksonder@gmail.com>
2017-09-08 17:31:22 +0200

    demo

---
+++ LambdaDemo/LambdaDemo/Auto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaDemo
+{
+    class Auto
+    {
+        public string Merk { get; set; }
+        public double Prijs { get; set; }
+        public int Bouwjaar { get; set; }
+
+        public Uitvoering Uitvoering { get; set; }
+    }
+
+    enum Uitvoering
+    {
+        Hatchback, Station, Landrover, Cabrio, Sedan
+    }
+}
+
---
+++ LambdaDemo/LambdaDemo/Garage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaDemo
+{
+    class Garage
+    {
+        public List<Auto> garage = new List<Auto>();
+
+        public void Filter(double prijs, int jaar, string merk, Uitvoering uitvoering)
+        {
+            IEnumerable<Auto> autos = garage
+                .Where(arg => arg.Prijs < prijs)
+                .Where(a => a.Bouwjaar > jaar)
+                .Where(a => a.Merk == merk)
+                .Where(a => a.Uitvoering == uitvoering)
+                .OrderBy(a => a.Prijs)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Het meegeven van een functie heet een 'delegate', callback
+        /// </summary>
+        /// <param name="arg"></param>
+        /// <returns></returns>
+        private bool PrijsIsLowerThan(Auto arg)
+        {
+            return arg.Prijs < 1000;
+        }
+    }
+}
